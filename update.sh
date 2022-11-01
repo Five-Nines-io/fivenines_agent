8482331c8bf88229d8f5508729bde51b2111d12c
Sébastien Puyet <sebastien@siit.io>
2022-11-01 22:46:57 +0100

    Improve comments

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ sudo five_nines_client/venv/bin/pip3 install -r requirements.txt
 # Copy the new service file
 sudo cp /opt/five_nines_client/five-nines-client.service /etc/systemd/system/
 
-# Reload the service files to include the five-nines-client service
+# Reload the service files
 sudo systemctl daemon-reload
 
 # Restart the five-nines-client
