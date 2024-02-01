679969c0998d81545a4232d21c34500cc24a4fb0
Sébastien Puyet <sebastienpuyet@gmail.com>
2024-02-01 16:57:55 +0100

    tese with new format

--- fivenines_setup.sh
+++ fivenines_setup.sh
@@ -12,22 +12,31 @@ echo -n "$1" | sudo tee /etc/fivenines_agent/TOKEN > /dev/null
 
 # Determine the package manager and install dependencies
 if [ -x "$(command -v apt-get)" ]; then
-  sudo apt-get install -y python3
+  sudo apt-get update
+  sudo apt-get install -y python3 python3-pip
 elif [ -x "$(command -v yum)" ]; then
   sudo yum update
-  sudo yum install -y python3
+  sudo yum install -y python3 python3-pip
 elif [ -x "$(command -v pacman)" ]; then
   sudo pacman -Syu
-  sudo pacman -S --noconfirm python
+  sudo pacman -S --noconfirm python python-pip
 else
   echo 'Error: No package manager found'
   exit 1
 fi
 
-# Copy the service file
-sudo cp fivenines-agent.service /etc/systemd/system/
+useradd --system --user-group --key USERGROUPS_ENAB=yes -M fivenines --shell /bin/false
 
-# Reload the service files to include the fivenines-agent service
+# Install the agent
+sudo python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple fivenines_agent
+
+# Download the service file
+wget https://raw.githubusercontent.com/Five-Nines-io/five_nines_agent/develop/fivenines-agent.service -O fivenines-agent.service
+
+# Move the service file to the systemd directory
+sudo mv fivenines-agent.service /etc/systemd/system/
+
+# Reload the service files to include the new fivenines-agent service
 sudo systemctl daemon-reload
 
 # Enable fivenines-agent service on every reboot
