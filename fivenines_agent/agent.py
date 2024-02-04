dab636a1905e1198a3896f723aa8119490c74e30
Sébastien Puyet <sebastienpuyet@gmail.com>
2024-02-04 20:09:36 +0100

    refactor env management + add ipv6 retrieval 🎉

--- fivenines_agent/agent.py
+++ fivenines_agent/agent.py
@@ -10,31 +10,23 @@ import json
 import platform
 import psutil
 
+from fivenines_agent.env import api_url, debug_mode
 from fivenines_agent.cpu import cpu_data, cpu_model
-from fivenines_agent.ip import ipv4
+from fivenines_agent.ip import ipv4, ipv6
 from fivenines_agent.network import network
 from fivenines_agent.partitions import partitions_metadata, partitions_usage
 from fivenines_agent.processes import processes
 from fivenines_agent.disks import io
 from fivenines_agent.files import file_handles_used, file_handles_limit
 
-from dotenv import load_dotenv
-
 CONFIG_DIR = "/etc/fivenines_agent"
+from dotenv import load_dotenv
 load_dotenv(dotenv_path=f'{CONFIG_DIR}/.env')
 
 class Agent:
     def __init__(self):
         for file in ["TOKEN"]:
             self.load_file(file)
-
-        default_env = {
-            "API_URL": 'api.five-nines.io',
-            "DEBUG_MODE": False
-        }
-
-        for env, default in default_env.items():
-            self.load_env(env, default)
 
         self.version = '0.1.2'
         self.config = {"request_options": {"timeout": 5}}
@@ -48,15 +40,6 @@ class Agent:
         except FileNotFoundError:
             print(f'{file} file is missing')
             sys.exit(2)
-
-    def load_env(self, env, default):
-        value = os.environ.get(env)
-        if value is None:
-            setattr(self, env.lower(), default)
-        else:
-            if value.isnumeric():
-                value = int(value)
-            setattr(self, env.lower(), value)
 
     def run(self):
         wd = systemd_watchdog.watchdog()
@@ -94,6 +77,8 @@ class Agent:
 
             if self.config['ipv4']:
                 data['ipv4'] = ipv4()
+            if self.config['ipv6']:
+                data['ipv6'] = ipv6()
 
             if self.config['network']:
                 data['network'] = network()
@@ -119,14 +104,14 @@ class Agent:
         if sleep_time < 0:
             sleep_time = 0
 
-        if self.debug_mode:
+        if debug_mode():
             print(f'Sleeping for {sleep_time} seconds')
         time.sleep(sleep_time)
 
     def ping(self, host):
         with os.popen(f'ping -c 1 {host} | grep "time=" | cut -d " " -f7 | cut -d "=" -f2', 'r') as f:
             result = f.read().rstrip('\n')
-        if self.debug_mode:
+        if debug_mode():
             print(f'ping_{host}: {repr(result)}')
 
         if result != '':
@@ -135,13 +120,13 @@ class Agent:
     def sync(self, data):
         try:
             conn = http.client.HTTPSConnection(
-                self.api_url, timeout=self.config['request_options']['timeout'])
+                api_url(), timeout=self.config['request_options']['timeout'])
             res = conn.request('POST', '/collect', json.dumps(data), {
                                'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json'})
             res = conn.getresponse()
             body = res.read().decode("utf-8")
 
-            if self.debug_mode:
+            if debug_mode():
                 print(f'Status: {res.status}')
                 print(f'Response: {body}')
 
--- fivenines_agent/ip.py
+++ fivenines_agent/ip.py
@@ -1,15 +1,33 @@
-import http
+import os
 import sys
 import traceback
 
+from fivenines_agent.env import debug_mode
+
 def ipv4():
   try:
-    conn = http.client.HTTPSConnection('ip.fivenines.io', timeout=5)
-    res = conn.request('GET', '/')
-    res = conn.getresponse()
-    body = res.read().decode("utf-8")
-    return body
+    with os.popen('curl -4 -s https://ip.fivenines.io', 'r') as f:
+      result = f.read().rstrip('\n')
+    if debug_mode():
+      print(f'ipv4: {repr(result)}')
 
+    if result != '':
+      return result
   except Exception as e:
+    print('IPv4 address could not be retrieved')
+    print(e, file=sys.stderr)
+    print(traceback.print_exc(), file=sys.stderr)
+
+def ipv6():
+  try:
+    with os.popen('curl -6 -s https://ip.fivenines.io', 'r') as f:
+      result = f.read().rstrip('\n')
+    if debug_mode():
+      print(f'ipv6: {repr(result)}')
+
+    if result != '':
+      return result
+  except Exception as e:
+    print('IPv6 address could not be retrieved')
     print(e, file=sys.stderr)
     print(traceback.print_exc(), file=sys.stderr)
