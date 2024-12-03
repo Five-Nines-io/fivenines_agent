e06361ffd770714e83ade2a6b2ac128e122821c0
Sébastien Puyet <sebastienpuyet@gmail.com>
2024-12-03 13:45:44 +0100

    v0.0.2 - improve errors management

--- fivenines_agent/synchronizer.py
+++ fivenines_agent/synchronizer.py
@@ -11,7 +11,7 @@ class Synchronizer(Thread):
         Thread.__init__(self)
         self.config_lock = Lock()
         self.token = token
-        self.config = { 'request_options': { 'timeout': 5, 'retry': 3, 'retry_interval': 5 } }
+        self.config = { 'enabled': True, 'request_options': { 'timeout': 5, 'retry': 3, 'retry_interval': 5 } }
         self.queue = queue
 
         self.send_request({'get_config': True})
@@ -49,9 +49,8 @@ class Synchronizer(Thread):
                     raise Exception(f'HTTP {res.status}: {body}')
             except Exception as e:
                 try_count += 1
-                if debug_mode():
-                    print(f'Synchronizer Error: {e}')
-                    print(f'Retrying in {self.config["request_options"]["retry_interval"] * try_count} seconds')
+                print(f'Synchronizer Error: {e}')
+                print(f'Retrying in {self.config["request_options"]["retry_interval"] * try_count} seconds')
                 time.sleep(self.config['request_options']['retry_interval'] * try_count)
 
     def get_config(self):
