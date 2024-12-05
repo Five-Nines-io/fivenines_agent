aac1a08e811be913fda07bb1cc8e71493fb9d812
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2024-12-05 17:15:12 +0800

    update py2exe.sh script: update shebang, enhance virtualenv installation, and improve build process

--- py2exe.sh
+++ py2exe.sh
@@ -1,4 +1,4 @@
-#!bash
+#!/usr/bin/env bash
 
 # IMPORTANT: PyInstaller need to be installed in the virtual environment (venv), so it can find the dependencies within the venv.
 
@@ -9,16 +9,26 @@ else
 fi
 
 #
-# Install virtualenv
+# Install and enable virtualenv
 #
 
+echo "Installing and enabling virtualenv"
+
 #sudo apt install python3-virtualenv
-virtualenv venv
-source venv/bin/activate
+python3 -m pip install --user virtualenv
+# virtualenv venv
+python3 -m venv venv
+
+# search for the `activate` script in the venv directory due to the `posix_local` and `posix_prefix` prefixes changes
+ACTIVATE_SCRIPT=$(find venv -name activate)
+source $ACTIVATE_SCRIPT
 
 #
 # Install dependencies with poetry inside the virtual environment
 #
+
+echo "Installing poetry and dependencies"
+python3 -m pip install poetry==1.8.4
 
 # It's important to disable virtualenvs.create, so pyinstaller can find the dependencies within the same virtual environment.
 poetry config virtualenvs.create false
@@ -27,12 +37,18 @@ poetry install --no-interaction
 # poetry add -G dev poetry-plugin-export
 # poetry add -G dev pyinstaller
 
-# Just for backward compatibility with the old build process
+#
+# Export dependencies to requirements.txt for backward compatibility with the old build process
+#
+
+echo "Exporting dependencies to requirements.txt for backward compatibility with the old build process"
 poetry export --without-hashes -o requirements.txt
 
 #
 # Build the executable
 #
+
+echo "Building the executable"
 
 mkdir -p build
 poetry run pyinstaller \
@@ -45,8 +61,13 @@ poetry run pyinstaller \
         --clean \
         ./py2exe_entrypoint.py
 
-# poetry config virtualenvs.create true
+#
+# Clean up
+#
 
-# clean up
+echo "Cleaning up"
 rm -rf ./build/tmp ./build/*.spec;
 
+echo "Resetting environment"
+poetry config virtualenvs.create true
+deactivate
