c4c6d761145241efd36c52cbc5b029b8e8de70a2
Sébastien Puyet <sebastien@puyet.fr>
2025-01-11 01:09:43 +0100

    update setup script + embed openssl + ping api on setup

--- fivenines_setup.sh
+++ fivenines_setup.sh
@@ -32,47 +32,9 @@ if ! id -u fivenines >/dev/null 2>&1; then
   sudo useradd --system --user-group --key USERGROUPS_ENAB=yes fivenines --shell /bin/false --create-home -b /opt/
 fi
 
-# Determine the package manager and install dependencies
-if [ -x "$(command -v apt-get)" ]; then
-  echo "apt-get found"
-  sudo apt-get update
-  sudo apt-get install -y python3 pipx gcc
-elif [ -x "$(command -v yum)" ]; then
-  echo "yum found"
-  sudo yum update
-  sudo yum install -y python3
-
-  # Install pipx through pip3 if yum doesn't have it
-  if ! sudo yum info pipx >/dev/null 2>&1; then
-    sudo yum install -y python3-pip gcc
-    sudo su - fivenines -s /bin/bash -c 'python3 -m pip install --user pipx'
-  else
-    sudo yum install -y pipx
-  fi
-
-elif [ -x "$(command -v pacman)" ]; then
-  echo "pacman found"
-  sudo pacman -Syu
-  sudo pacman -S --noconfirm python python-pipx gcc
-else
-  exit_with_contact "No package manager found"
-fi
-
-if ! python3 --version | cut -d " " -f2 | grep -E '^3.(6|7|8|9|1.+|2.+)..+$' > /dev/null; then
-  echo "Error: Python version must be 3.6 or higher, you have $(python3 --version | cut -d " " -f2)"
-  exit_with_contact "Python version must be 3.6 or higher"
-  exit 1
-fi
-
-# Install the agent
-sudo su - fivenines -s /bin/bash -c 'python3 -m pipx install --index-url https://pypi.org/simple/ --pip-args="--extra-index-url https://pypi.org/simple" fivenines_agent'
-# Check that the package is installed
-sudo su - fivenines -s /bin/bash -c 'pipx list | grep -q fivenines_agent'
-
-# Get the exit status of the pipx command
-if [ $? -ne 0 ]; then
-  exit_with_contact "Failed to install the fivenines_agent package"
-fi
+# Download the agent
+wget https://github.com/Five-Nines-io/fivenines_agent/releases/download/v.1.0.6/fivenines-agent-linux-amd64 -O /opt/fivenines/fivenines_agent
+chmod +x /opt/fivenines/fivenines_agent
 
 # Download the service file
 wget https://raw.githubusercontent.com/Five-Nines-io/five_nines_agent/main/fivenines-agent.service -O fivenines-agent.service
@@ -80,7 +42,7 @@ wget https://raw.githubusercontent.com/Five-Nines-io/five_nines_agent/main/fiven
 # Move the service file to the systemd directory
 sudo mv fivenines-agent.service /etc/systemd/system/
 
-hosts=("asia.fivenines.io" "eu.fivenines.io" "us.fivenines.io")
+hosts=("asia.fivenines.io" "eu.fivenines.io" "us.fivenines.io" "api.fivenines.io")
 
 # Loop through each host and ping once
 for host in "${hosts[@]}"; do
