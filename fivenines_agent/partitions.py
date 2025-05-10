d5e26cb5e3c49eefe43976b4673ed7a4130e3323
Sébastien Puyet <sebastien@puyet.fr>
2025-05-10 20:41:02 +0200

    focus on filesystem to skip partitions

--- fivenines_agent/partitions.py
+++ fivenines_agent/partitions.py
@@ -1,17 +1,12 @@
 import psutil
 from fivenines_agent.env import debug_mode
 
-
-IGNORED_DEVICES = ['/loop', '/snap']
-IGNORED_FS = ['squashfs', 'cagefs-skeleton']
+IGNORED_FS = ['squashfs', 'cagefs-skeleton', 'overlay', 'devtmpfs', 'tmpfs', 'loop']
 
 def partitions_metadata():
     partitions_metadata = []
 
     for part in psutil.disk_partitions(False):
-        if part.device.startswith(tuple(IGNORED_DEVICES)):
-            continue
-
         if part.fstype in IGNORED_FS:
             continue
 
@@ -29,9 +24,6 @@ def partitions_usage():
 
     for _, v in enumerate(psutil.disk_partitions(all=False)):
         try:
-            if v.device.startswith(tuple(IGNORED_DEVICES)):
-                continue
-
             if v.fstype in IGNORED_FS:
                 continue
 
